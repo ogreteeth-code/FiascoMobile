ec4cab552a3c86c5d1f2ab3bdf4d29237ced1a83
Tim Rodriguez <info@epistech.com>
2014-09-06 15:48:53 -0400

    DOM hack
    using DOM to store global variables/page depth for back button

--- js/fiasco.js
+++ js/fiasco.js
@@ -1,13 +1,13 @@
 function loadHomeScreen() {
+	document.getElementById("homescreen").setAttribute("value","0");
 	$('#IOSContainer').load('tmpl/_homescreen.tmpl.html');
 	setTimeout(function(){scrollTo(0,0)},1);
-	var homescreen = 0;
 }
 
 function loadAboutApp() {
+	document.getElementById("homescreen").setAttribute("value","-1");
 	$('#IOSContainer').load('tmpl/_aboutapp.tmpl.html');
 	setTimeout(function(){scrollTo(0,0)},1);
-	var homescreen = -1;
 }
 
 function subSwap(folioID) {
@@ -20,31 +20,31 @@ function subSwap(folioID) {
 }
 
 function loadJSONPlayset(title) {
+	document.getElementById("homescreen").setAttribute("value","-2");
 	$.getJSON("playsets/" + title,function(data) {$("#playsetProper").tmpl(data).replaceAll("#IOSContainer");});
-	var homescreen = -2;
 }
 
 function getPlayset(playset) {
+	document.getElementById("homescreen").setAttribute("value","-1");
 	currentPlayset = playset;
 	// console.log(playset);
 	title = {"filename": playset};
 	$.getJSON("playsets/" + playset,function(data) {data = title.merge(data); $("#playsetTitleScreen").tmpl(data).replaceAll("#IOSContainer");});
 	setTimeout(function(){scrollTo(0,0)},1);
-	var homescreen = -1;
 }
 
 function onPhoneReady(){ 
   document.addEventListener("backbutton", function(){ //hardware backbutton
+	var homescreen = document.getElementById("homescreen").getAttribute("value");
 	alert(homescreen);
   	if (homescreen == 0) {
-  		alert("homescreen == 0");
   		return true;
   	} else if (homescreen == -2) {
   		alert("homescreen == -2");
   		getPlayset(currentPlayset);
   		return false;
   	} else {
-  		alert("return home now");
+  		// alert("return home now");
 	    loadHomeScreen();
 	    return false;
   	}
